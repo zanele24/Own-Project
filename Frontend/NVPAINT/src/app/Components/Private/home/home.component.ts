d3af3f008e504367c297a27609aa3231eb290797
DA_M1_46 <da_m1_46@DA-LEARNERs-MacBook-Air.local>
2022-08-02 16:32:21 +0200

    changes

--- Frontend/NVPAINT/src/app/Components/Private/home/home.component.ts
+++ Frontend/NVPAINT/src/app/Components/Private/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GalleriaModule} from 'primeng/galleria';
 
 @Component({
   selector: 'app-home',
--- Frontend/NVPAINT/src/app/app.module.ts
+++ Frontend/NVPAINT/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {GalleriaModule} from 'primeng/galleria';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { SigninComponent } from './Components/Private/signin/signin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GalleriaModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
